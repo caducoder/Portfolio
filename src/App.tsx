b29f16eef43226947f170d1161e666dbd2f5a6e9
caducoder <cadudrop@hotmail.com>
2022-07-25 16:38:16 -0300

    Create skills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar"
 import Banner from "./components/Banner"
+import Skills from "./components/Skills"
 
 function App() {
   // remover arquivo App.scss, caso não coloque nenhum estilo
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   )
 }
---
+++ src/components/Skills/index.tsx
@@ -0,0 +1,105 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import Javascript from '../../assets/img/js.svg'
+import React from '../../assets/img/react.svg'
+import HTML from '../../assets/img/html.svg'
+import CSS from '../../assets/img/css.svg'
+import Java from '../../assets/img/java.svg'
+import Git from '../../assets/img/git.svg'
+import Docker from '../../assets/img/docker.svg'
+import Sass from '../../assets/img/sass.svg'
+import Linux from '../../assets/img/linux.svg'
+import Firebase from '../../assets/img/firebase.svg'
+import Typescript from '../../assets/img/typescript.svg'
+import Postgres from '../../assets/img/postgres.svg'
+
+import './Skills.scss'
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+
+function Skills() {
+  return (
+    <section className='skill' id='skills'>
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-bx">
+              <h2>Habilidades</h2>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <Carousel responsive={responsive} infinite={true} className='skill-slider'>
+                <div className="item">
+                  <img src={HTML} alt="Skill Image" />
+                  <h5>HTML</h5>
+                </div>
+                <div className="item">
+                  <img src={CSS} alt="Skill Image" />
+                  <h5>CSS</h5>
+                </div>
+                <div className="item">
+                  <img src={Javascript} alt="Skill Image" />
+                  <h5>JAVASCRIPT</h5>
+                </div>
+                <div className="item">
+                  <img src={Typescript} alt="Skill Image" />
+                  <h5>TYPESCRIPT</h5>
+                </div>
+                <div className="item">
+                  <img src={React} alt="Skill Image" />
+                  <h5>REACTJS</h5>
+                </div>
+                <div className="item">
+                  <img src={Java} alt="Skill Image" />
+                  <h5>JAVA</h5>
+                </div>
+                <div className="item">
+                  <img src={Git} alt="Skill Image" />
+                  <h5>GIT</h5>
+                </div>
+                <div className="item">
+                  <img src={Docker} alt="Skill Image" />
+                  <h5>DOCKER</h5>
+                </div>
+                <div className="item">
+                  <img src={Sass} alt="Skill Image" />
+                  <h5>SASS</h5>
+                </div>
+                <div className="item">
+                  <img src={Linux} alt="Skill Image" />
+                  <h5>LINUX</h5>
+                </div>
+                <div className="item">
+                  <img src={Firebase} alt="Skill Image" />
+                  <h5>FIREBASE</h5>
+                </div>
+                <div className="item">
+                  <img src={Postgres} alt="Skill Image" />
+                  <h5>POSTGRES</h5>
+                </div>
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
+
+export default Skills;
