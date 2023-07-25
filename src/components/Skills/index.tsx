5ee9c6b0ed04adc7c120f364828ee6ac867a9640
caducoder <cadudrop@hotmail.com>
2023-07-25 10:35:12 -0300

    refactor: create json for tools info

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,18 +1,7 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import Javascript from '../../assets/img/js.svg'
-import React from '../../assets/img/react.svg'
-import HTML from '../../assets/img/html.svg'
-import CSS from '../../assets/img/css.svg'
-import Java from '../../assets/img/java.svg'
-import Git from '../../assets/img/git.svg'
-import Docker from '../../assets/img/docker.svg'
-import Sass from '../../assets/img/sass.svg'
-import Linux from '../../assets/img/linux.svg'
-import Firebase from '../../assets/img/firebase.svg'
-import Typescript from '../../assets/img/typescript.svg'
-import Postgres from '../../assets/img/postgres.svg'
+import skills from './skills.json'
 
 import './Skills.scss'
 
@@ -52,54 +41,12 @@ function Skills() {
                 autoPlay
                 autoPlaySpeed={5000}
               >
-                <div className="item">
-                  <img src={HTML} alt="Skill Image" />
-                  <h5>HTML</h5>
-                </div>
-                <div className="item">
-                  <img src={CSS} alt="Skill Image" />
-                  <h5>CSS</h5>
-                </div>
-                <div className="item">
-                  <img src={Javascript} alt="Skill Image" />
-                  <h5>JAVASCRIPT</h5>
-                </div>
-                <div className="item">
-                  <img src={Typescript} alt="Skill Image" />
-                  <h5>TYPESCRIPT</h5>
-                </div>
-                <div className="item">
-                  <img src={React} alt="Skill Image" />
-                  <h5>REACTJS</h5>
-                </div>
-                <div className="item">
-                  <img src={Java} alt="Skill Image" />
-                  <h5>JAVA</h5>
-                </div>
-                <div className="item">
-                  <img src={Git} alt="Skill Image" />
-                  <h5>GIT</h5>
-                </div>
-                <div className="item">
-                  <img src={Docker} alt="Skill Image" />
-                  <h5>DOCKER</h5>
-                </div>
-                <div className="item">
-                  <img src={Sass} alt="Skill Image" />
-                  <h5>SASS</h5>
-                </div>
-                <div className="item">
-                  <img src={Linux} alt="Skill Image" />
-                  <h5>LINUX</h5>
-                </div>
-                <div className="item">
-                  <img src={Firebase} alt="Skill Image" />
-                  <h5>FIREBASE</h5>
-                </div>
-                <div className="item">
-                  <img src={Postgres} alt="Skill Image" />
-                  <h5>POSTGRES</h5>
-                </div>
+                {skills.map(skill => (
+                  <div className='item'>
+                    <img src={skill.src} alt={skill.alt} />
+                    <h5>{skill.title}</h5>
+                  </div>
+                ))}
               </Carousel>
             </div>
           </Col>
